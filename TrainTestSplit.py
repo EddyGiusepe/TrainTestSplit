d48d17b3102778802a41d8c31f123fce9e5698ea
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-12-15 00:02:56 -0300

    atualizando código

--- TrainTestSplit.py
+++ TrainTestSplit.py
@@ -1,5 +1,6 @@
 '''
 Data Scientist Jr.: Dr.Eddy Giusepe Chirinos Isidro
+Link de estudo: https://www.youtube.com/watch?v=8Jfc1w8wwII
 '''
 
 # Importamos as nossas bibliotecas:
@@ -18,6 +19,29 @@ print(iris.data)
 print("Mostramos, agora, nossas etiquetas - target: ")
 print(iris.target.shape)
 print(iris.target)
+
+print("Escrevemos nossos Dados de train e test: ")
+# y_train, são nossas labels as quais por default são embaralhadas
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target)
+print("Escrevemos o shape de nossos Dados para ver como foi feito a divisão: ")
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+print("y_train, embaralhada será: ", y_train)
+
+print("Manipulando mais um pouco nossos Dados: ")
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.30)
+# Também podemos colocar:  test_size=50 --> A quantidade para teste
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+
+
+
 
 
 
